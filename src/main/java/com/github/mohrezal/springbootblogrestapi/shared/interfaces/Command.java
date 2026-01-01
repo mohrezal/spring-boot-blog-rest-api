53b04d5a4ecd1eacc9ddf6760efea692439f3bf1
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2026-01-01 12:56:03 +0400

    feat: add query and command

---
+++ src/main/java/com/github/mohrezal/springbootblogrestapi/shared/interfaces/Command.java
@@ -0,0 +1,8 @@
+package com.github.mohrezal.springbootblogrestapi.shared.interfaces;
+
+public interface Command<P, R> {
+    R execute(P params);
+
+    default void validate(P params) {
+    }
+}
---
+++ src/main/java/com/github/mohrezal/springbootblogrestapi/shared/interfaces/Query.java
@@ -0,0 +1,8 @@
+package com.github.mohrezal.springbootblogrestapi.shared.interfaces;
+
+public interface Query<P, R>  {
+    R execute(P params);
+
+    default void validate(P params) {
+    }
+}
