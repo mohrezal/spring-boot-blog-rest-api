5309da5164c05ec69b40b8fb2b2c3eb32a48a3a7
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2026-01-05 13:33:32 +0400

    feat: add category summary and mapper

---
+++ src/main/java/com/github/mohrezal/springbootblogrestapi/domains/categories/dtos/CategorySummary.java
@@ -0,0 +1,23 @@
+package com.github.mohrezal.springbootblogrestapi.domains.categories.dtos;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategorySummary {
+    private UUID id;
+    private String name;
+    private String slug;
+    private String description;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+}
