0dbf971fb10138f887f065371014ce2a5b147ef4
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2026-01-22 18:26:19 +0400

    feat: add some important packages and setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,12 +40,16 @@ dependencies {
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
     implementation("org.mapstruct:mapstruct:1.6.3")
+    implementation("software.amazon.awssdk:s3:2.25.60")
+    implementation("org.apache.tika:tika-core:2.9.2")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-liquibase-test")
@@ -53,7 +57,16 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("com.h2database:h2")
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.4"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+
+    testImplementation("org.awaitility:awaitility:4.2.2")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("net.datafaker:datafaker:2.4.2")
 }
 
 tasks.withType<Test> {
