74af202103d4f75e32618e8073d2cca0d5223fda
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2026-08-14 15:16:00 +0400

    feat: add RequiresPermission annotation

--- services/api/src/main/java/com/github/mohrezal/api/config/security/SecurityConfig.java
+++ services/api/src/main/java/com/github/mohrezal/api/config/security/SecurityConfig.java
@@ -30,7 +30,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
---
+++ services/api/src/main/java/com/github/mohrezal/api/shared/annotations/RequiresPermission.java
@@ -0,0 +1,15 @@
+package com.github.mohrezal.api.shared.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('{value}')")
+public @interface RequiresPermission {
+
+    String value();
+}
